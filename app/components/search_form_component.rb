bed1965880bf6d2593da52b6b9408e771740b636
lucas araujo <lucas.trabalho.araujo@gmail.com>
2026-04-22 19:16:59 -0300

    feat(phase-3): SearchFormComponent + preview + spec

---
+++ app/components/search_form_component.rb
@@ -0,0 +1,18 @@
+class SearchFormComponent < ViewComponent::Base
+  CABIN_CLASSES = %w[economy premium_economy business first].freeze
+
+  def initialize(autocomplete_url:, search_params: {})
+    @autocomplete_url = autocomplete_url
+    @search_params = search_params.to_h.with_indifferent_access
+  end
+
+  attr_reader :autocomplete_url
+
+  def value_for(key, default = nil)
+    @search_params[key].presence || default
+  end
+
+  def round_trip?
+    value_for(:trip_type) == "round_trip"
+  end
+end
