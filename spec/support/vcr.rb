456f9befd93e69a9fc0b0e05e5e142b95687d743
lucas araujo <lucas.trabalho.araujo@gmail.com>
2026-04-23 09:15:04 -0300

    chore(phase-4): vcr filter com nested keys (duffel/amadeus/telegram etc)

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -11,18 +11,16 @@ VCR.configure do |config|
     match_requests_on: [:method, :uri, :body]
   }
 
-  %w[
-    duffel_api_key
-    amadeus_client_id
-    amadeus_client_secret
-    seats_aero_api_key
-    telegram_bot_token
-    telegram_owner_chat_id
-    sentry_dsn
-  ].each do |secret|
-    config.filter_sensitive_data("<#{secret.upcase}>") do
-      Rails.application.credentials.dig(secret.to_sym).to_s
-    end
+  {
+    "DUFFEL_API_KEY" => -> { Rails.application.credentials.dig(:duffel, :api_key).to_s },
+    "AMADEUS_CLIENT_ID" => -> { Rails.application.credentials.dig(:amadeus, :client_id).to_s },
+    "AMADEUS_CLIENT_SECRET" => -> { Rails.application.credentials.dig(:amadeus, :client_secret).to_s },
+    "SEATS_AERO_API_KEY" => -> { Rails.application.credentials.dig(:seats_aero, :api_key).to_s },
+    "TELEGRAM_BOT_TOKEN" => -> { Rails.application.credentials.dig(:telegram, :bot_token).to_s },
+    "TELEGRAM_OWNER_CHAT_ID" => -> { Rails.application.credentials.dig(:telegram, :owner_chat_id).to_s },
+    "SENTRY_DSN" => -> { Rails.application.credentials.dig(:sentry_dsn).to_s }
+  }.each do |placeholder, resolver|
+    config.filter_sensitive_data("<#{placeholder}>") { resolver.call }
   end
 end
 
