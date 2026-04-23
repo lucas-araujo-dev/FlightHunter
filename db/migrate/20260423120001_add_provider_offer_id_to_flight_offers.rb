beffb5d40e05b68e62dbbcd09c4bda9e8cbd286f
lucas araujo <lucas.trabalho.araujo@gmail.com>
2026-04-23 09:25:30 -0300

    chore(phase-4): standardrb autofix na migration do provider_offer_id

--- db/migrate/20260423120001_add_provider_offer_id_to_flight_offers.rb
+++ db/migrate/20260423120001_add_provider_offer_id_to_flight_offers.rb
@@ -3,6 +3,6 @@ class AddProviderOfferIdToFlightOffers < ActiveRecord::Migration[8.1]
     add_column :flight_offers, :provider_offer_id, :string
     change_column_null :flight_offers, :provider_offer_id, false
     add_index :flight_offers, [:provider, :provider_offer_id],
-              unique: true, name: "idx_flight_offers_provider_offer_unique"
+      unique: true, name: "idx_flight_offers_provider_offer_unique"
   end
 end
